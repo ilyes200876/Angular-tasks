caa34e2f668b879d3c7c13f36097591fe814c21e
ilyes200876 <ilyes200876@live.fr>
2023-09-08 14:40:13 +0200

    create tzsk project znd components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListTachesComponent } from './list-taches/list-taches.component';
+import { HeaderComponent } from './header/header.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListTachesComponent,
+    HeaderComponent,
+    BitcoinComponent
   ],
   imports: [
     BrowserModule,
